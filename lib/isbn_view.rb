754dbee718647e366f110f5d8720d80328dd418a
Daniel Costa <dancosta.id@gmail.com>
2022-03-29 21:07:43 -0300

    I've finished the view part of the app. It is finally ready

--- lib/isbn_view.rb
+++ lib/isbn_view.rb
@@ -9,8 +9,25 @@ class IsbnView
     gets.chomp.to_i.digits.reverse
   end
 
-  def display_final_isbn(isbn)
-    puts 'Here is your isbn number:'
+  def display_final_isbn(isbn, multiplied_digits, sum_of_digits, mod_of_digits)
+    puts ''
+    puts "Here's your number: #{isbn}"
+    sleep(0.2)
+    puts ''
+    puts "I've put them in an array: #{multiplied_digits}..."
+    sleep(0.8)
+    puts ''
+    puts "Now, I'm multiplying even numbers by 1 and odd numbers by 3"
+    sleep(0.4)
+    puts "After adding them up, this is what I've got: #{sum_of_digits}"
+    puts ''
+    sleep(0.8)
+    puts "Now, it's time to check the mod 10..."
+    sleep(0.4)
+    puts "Here it is: #{mod_of_digits}"
+    puts ''
+    sleep(0.8)
+    puts 'And finally... Here is your isbn number:'
     puts isbn.digits.join
   end
 end
